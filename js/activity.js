b21d010e936c49776b6d10dc3a8e270859ff133c
Alice Kim <alice.kim@qualia.com>
2025-06-09 10:31:46 -0700

    show the list of the box

--- js/activity.js
+++ js/activity.js
@@ -1,16 +1,33 @@
 $(document).ready(function() {
-  // Make selectable cells clickable
-  $("tbody td").each(function() {
-      var cellText = $(this).text().trim();
+    $("tbody td").each(function() {
+        var cellText = $(this).text().trim();
 
-      // If cell is NOT "Not Available", add selectable class and hand cursor
-      if (cellText !== "Not Available") {
-          $(this).addClass("selectable").css("cursor", "pointer");
-      }
-  });
+        if (cellText !== "Not Available") {
+            $(this).addClass("selectable").css("cursor", "pointer");
+        }
+    });
 
-  // Toggle selection on click
-  $("tbody td.selectable").on("click", function() {
-      $(this).toggleClass("selected");
-  });
+    $("tbody td.selectable").on("click", function() {
+        $(this).toggleClass("selected");
+        updateSelectedActivities();
+    });
+
+    function updateSelectedActivities() {
+        var selectedList = $("#activity-list");
+        selectedList.empty();
+
+        $("tbody td.selected").each(function() {
+            var activityName = $(this).text().trim();
+            var columnIndex = $(this).index();
+            var cliffName = $("thead th").eq(columnIndex).text().trim();
+
+            selectedList.append("<li>" + activityName + " (" + cliffName + ")</li>");
+        });
+
+        if ($("tbody td.selected").length > 0) {
+            $("#selected-activities").show();
+        } else {
+            $("#selected-activities").hide();
+        }
+    }
 });
